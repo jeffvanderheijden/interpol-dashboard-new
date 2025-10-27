ec57fcf7e038b489cab1e2f074089360745a4aaf
Jeff van der Heijden <jheijden@jheijdens-MacBook-Pro.local>
2025-10-27 14:07:40 +0100

    fixed broken redirect in protectedroute

--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -5,7 +5,7 @@ export default function ProtectedRoute({ children }) {
     const { user, loading } = useAuth();
 
     if (loading) return <div>Bezig met laden...</div>;
-    if (!user) return <Navigate to="/login" replace />;
+    if (!user) return <Navigate to="/" replace />;
 
     return children;
 }
