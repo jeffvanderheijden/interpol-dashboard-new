f659f129b7caa8f85c6adb557f36bc09663aeeb7
Jeff van der Heijden <jheijden@jheijdens-MacBook-Pro.local>
2025-11-14 14:50:33 +0100

    Fixing routing issue

--- src/App.jsx
+++ src/App.jsx
@@ -15,9 +15,9 @@ const App = () => {
                 <AuthProvider>
                     <Routes>
                         <Route path="/" element={<Login />} />
-                        <Route path="/training" element={<ProtectedRoute><Training /></ProtectedRoute>} />
-                        <Route path="/html-css" element={<ProtectedRoute><HtmlCss /></ProtectedRoute>} />
-                        <Route path="/javascript" element={<ProtectedRoute><Javascript /></ProtectedRoute>} />
+                        <Route path="/training/*" element={<ProtectedRoute><Training /></ProtectedRoute>} />
+                        <Route path="/html-css/*" element={<ProtectedRoute><HtmlCss /></ProtectedRoute>} />
+                        <Route path="/javascript/*" element={<ProtectedRoute><Javascript /></ProtectedRoute>} />
                         {/* TODO: Maak 404 pagina */}
                         <Route path="*" element={<div>Pagina niet gevonden</div>} /> 
                     </Routes>
