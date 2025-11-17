3722616b3696c8da365a4823f89d72b9e6bfbaae
Jeff van der Heijden <jheijden@jheijdens-MacBook-Pro.local>
2025-11-17 13:37:25 +0100

    Debugging broken post request, weird cors errors..

--- src/api/groups.js
+++ src/api/groups.js
@@ -3,15 +3,17 @@ import { API_BASE } from "./_config";
 export async function createGroup(teamPhoto, members) {
     const res = await fetch(`${API_BASE}/api/groups`, {
         method: "POST",
+        credentials: "include",
         headers: {
-            "Content-Type": "application/json",
-            "credentials": "include",
+            "Content-Type": "application/json"
         },
         body: JSON.stringify({ teamPhoto, members }),
     });
 
     if (!res.ok) {
-        throw new Error(`API error ${res.status}`);
+        const text = await res.text();
+        console.error("API ERROR:", text);
+        throw new Error("CreateGroup failed");
     }
 
     return res.json();
